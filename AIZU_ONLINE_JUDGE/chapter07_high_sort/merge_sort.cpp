8f3c23962b2b2ff3d47c2ba9134a0753399c174a
mizuki <nagoyamarshmallow@gmail.com>
2024-02-20 22:00:05 +0900

    2024/02/20

--- AIZU_ONLINE_JUDGE/chapter07_high_sort/merge_sort.cpp
+++ AIZU_ONLINE_JUDGE/chapter07_high_sort/merge_sort.cpp
@@ -1,14 +1,65 @@
 #include<iostream>
 using namespace std;
-void merge(int A[],int left,int mid,int right){
-    n1 = mid - left;
-    n2 = right - mid;
+#define MAX 500000
+#define SENTINEL 200000000
+int L[MAX/2 + 2],R[MAX/2 + 2];
+int cnt;
+
+
+void merge(int A[],int n,int left,int mid,int right){
+    int n1 = mid - left;
+    int n2 = right - mid;
     //L[0,1,2,...n1]とR[0,1,2,...n2]を生成
-    for(int i = 0, i < n1; i++){
-        L[i] = A[left + 1];
+    for(int i = 0; i < n1; i++){
+        L[i] = A[left + i];
     }
 
     for(int i = 0; i < n2; i++){
         R[i] = A[mid + i];
     }
+
+    L[n1] = R[n2] = SENTINEL;
+    int i = 0, j = 0;
+    for(int k = left; k < right; k++){
+        cnt++;
+        if(L[i] <= R[j]){
+            A[k] = L[i++];
+        }else{
+            A[k] = R[j++];
+        }
+    }
+}
+
+void mergeSort(int A[],int n,int left,int right){
+    if(left + 1 < right){
+        int mid = (left + right) / 2;
+        mergeSort(A,n,left,mid);
+        mergeSort(A,n,mid,right);
+        merge(A,n,left,mid,right);
+    }
+}
+
+int main(){
+    int A[MAX],n,i;
+    cnt = 0;
+
+    cin >> n;
+    for(int i = 0; i < n;i++){
+        cin >> A[i];
+    }
+
+    mergeSort(A,n,0,n);
+
+    for(int i = 0; i < n;i++){
+        if(i){
+            cout << " ";
+        }
+        cout << A[i];
+    }
+
+    cout << endl;
+
+    cout << cnt << endl;
+
+    return 0;
 }
